19acbf594bad6b7888610b32867a0b572ea14d7d
nseven07 <necipseven07@gmail.com>
2020-03-09 17:55:21 +0300

    codeck info page

---
+++ src/assets/js/console.js
@@ -0,0 +1,32 @@
+console.log("" +
+    "════════════════════════ஜ۩۞۩ஜ════════════════════════\n" +
+    "_________________Selam Ben Necip Seven_________________\n" +
+    "════════════════════════ஜ۩۞۩ஜ════════════════════════");
+console.log("" +
+    "════════════════════════ஜ۩۞۩ஜ════════════════════════\n" +
+    "____________Codeck Yazılım A.Ş de Stajyerim____________\n" +
+    "════════════════════════ஜ۩۞۩ஜ════════════════════════");
+console.log("" +
+    "════════════════════════ஜ۩۞۩ஜ════════════════════════\n" +
+    "__________Bizde Stajyerler Bile Böyle İş Yapar_________\n" +
+    "════════════════════════ஜ۩۞۩ஜ════════════════════════");
+
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+    document.getElementById("visible").style.overflow = "hidden";
+
+}
+
+function showPage() {
+
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("visible").style.overflow = "auto";
+
+
+}
+
+
+
