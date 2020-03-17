6efbbf5ae953fe6f9a3edc1d83d31648495cee45
Samed <samedcetin@orgino.com.tr>
2020-03-17 21:19:28 +0300

    Homepage Finish

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,11 @@
 function myFunction() {
-    const myVar = setTimeout(showPage, 3000);
-    document.getElementById("visible").style.overflow = "hidden";
+    // const myVar = setTimeout(showPage, 3000);
+    // document.getElementById("visible").style.overflow = "hidden";
 }
 
 function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("visible").style.overflow = "auto";
+    // document.getElementById("loader").style.display = "none";
+    // document.getElementById("visible").style.overflow = "auto";
 }
 
 $(document).ready(function () {
@@ -17,3 +17,4 @@ $(document).ready(function () {
         speed: 300,
     })
 });
+
