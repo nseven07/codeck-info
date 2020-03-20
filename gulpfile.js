ae0dc5dd2db6de216ede1d810f1f0ddf43d89638
nseven07 <necipseven07@gmail.com>
2020-03-20 22:02:21 +0300

    presentation button

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const gulp = require('gulp');
+
+
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const gsass = require('gulp-sass');
+const cssnano = require('cssnano');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const twig = require('gulp-twig');
+const concat = require('gulp-concat');
+const connect = require('gulp-connect');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify-es').default;
+const open = require('gulp-open');
+const header = require('gulp-header');
+const imagemin = require('gulp-imagemin');
+
+const templates = './src/presentation/';
+const dist = './dist/';
+const homePath = './src/assets/';
+const nodeModules = './node_modules/';
+const js = 'assets/js/  ';
+const css = 'assets/css/';
+const icons = 'assets/icons/';
+
+
+const sourceJs = [
+    nodeModules + 'jquery/dist/jquery.js',
+    nodeModules + 'wowjs/dist/wow.min.js',
+    nodeModules + 'bootstrap/dist/js/bootstrap.js',
+    nodeModules + 'slick-carousel/slick/slick.js',
+    nodeModules + '@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+    nodeModules + 'fullpage.js/dist/fullpage.js',
+    nodeModules + 'fullpage.js/dist/fullpage.extensions.min.js',
+    homePath + 'js/main.js',
+    homePath + 'js/console.js',
+];
+
+const sourceSass = [
+    homePath + "css/style.sass"
+];
+
+function compile() {
+    const twig = require('gulp-twig');
+    return gulp.src('./src/presentation/pages/*.html')
+        .pipe(twig({
+            base: './src/presentation'
+        }))
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
+}
+
+function scripts() {
+    return gulp.src(sourceJs)
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest(dist + 'assets/js'))
+        .pipe(connect.reload());
+}
+
+
+function scriptsDev() {
+    return gulp.src(sourceJs)
+        .pipe(concat('scripts.js'))
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest(dist + 'assets/js'))
+        .pipe(connect.reload())
+}
+
+
+function connectGulp() {
+    connect.server({
+        root: [dist + 'templates', 'dist'],
+        livereload: true
+    });
+}
+
+function clean() {
+    return del([dist + '**', '!' + dist])
+}
+
+function copyFiles() {
+    return gulp
+        .src(['./src/assets/img/**/*', './src/assets/icons/**/*', './src/assets/font/**/*', './src/assets/presentation/**/*'], {base: './src/'})
+        //.pipe(imagemin({interlaced: true, progressive: true, optimizationLevel: 5, svgoPlugins: [{removeViewBox: true}]}))
+        .pipe(gulp.dest(dist))
+}
+
+function style() {
+    return gulp.src(sourceSass)
+        .pipe(gsass({
+            includePaths: ['node_modules']
+        }))
+        .pipe(gulp.dest(dist + 'assets/css'))
+        .pipe(connect.reload());
+}
+
+function myWatchTasks() {
+    gulp.watch(['./src/assets/css/*.sass'], style);
+    gulp.watch(['./src/assets/js/*.js'], scriptsDev);
+    gulp.watch(['./src/presentation/**/*.html'], compile)
+}
+
+const dev = gulp.parallel(gulp.series(clean, style, copyFiles, scriptsDev, compile, connectGulp), myWatchTasks);
+const build = gulp.series(clean, style, copyFiles, scripts, compile,);
+
+
+exports.clean = clean;
+exports.copyFiles = copyFiles;
+exports.compile = compile;
+exports.build = build;
+exports.default = dev;
+exports.dev = dev;
+exports.style = style;
+
---
+++ src/assets/js/main.js
@@ -0,0 +1,141 @@
+$('#menuActive').click(function () {
+    $('.fullMenu, .menuItems').toggleClass("menu-btn-active");
+    $('.fullMenu').toggleClass('hidden');
+    $('#hamburgerMenu').toggleClass("menu-active");
+    $('#menuActive').addClass('kapat')
+
+});
+$(document).ready(function () {
+    let pathname, webApp, mobil, grafiktasarim;
+
+    pathname = $(location).attr('href');
+    webApp = pathname === 'http://localhost:8080/projects.html?webApp=true';
+    mobil = pathname === 'http://localhost:8080/projects.html?mobil=true';
+    grafiktasarim = pathname === 'http://localhost:8080/projects.html?grafiktasarim=true';
+
+    if (webApp) {
+        $('#webApp').addClass('btn-active').trigger("click");
+    } else {
+        $('#webApp').removeClass('btn-active');
+    }
+    if (mobil) {
+        $('#mobile').addClass('btn-active').trigger("click");
+    } else {
+        $('#mobile').removeClass('btn-active');
+    }
+    if (grafiktasarim) {
+        $('#grafiktasarim').addClass('btn-active').trigger("click");
+    } else {
+        $('#grafiktasarim').removeClass('btn-active');
+    }
+});
+//Slick
+$(document).ready(function () {
+    $('.staff-slider, .projectImages').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false,
+        speed: 300,
+        autoplay: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.mobil-logo').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+    });
+});
+//---------
+
+//Full Page Slider
+$(document).ready(function () {
+    const isHomePage = document.getElementById('home-page');
+    if (isHomePage) {
+        $('#home-page').fullpage({
+            //options here
+            autoScrolling: true,
+            navigation: true,
+            responsiveHeight: 600,
+            scrollHorizontally: true,
+            scrollBar: true,
+            dragAndMove: true,
+            touchSensitivity: 10,
+        });
+    }
+});
+//---------
+
+const projectPage = document.getElementById('project-page');
+
+if (projectPage) {
+    filterSelection("all");
+
+    function filterSelection(c) {
+        let x, i;
+        x = document.getElementsByClassName("filterDiv");
+        if (c === "all") c = "";
+        for (i = 0; i < x.length; i++) {
+            removeClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+        }
+    }
+
+    function addClass(element, name) {
+        let i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) === -1) {
+                element.className += " " + arr2[i];
+            }
+        }
+    }
+
+    function removeClass(element, name) {
+        let i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+                arr1.splice(arr1.indexOf(arr2[i]), 1);
+            }
+        }
+        element.className = arr1.join(" ");
+    }
+
+
+    const btnContainer = document.getElementById("buttons");
+    const btns = btnContainer.getElementsByClassName("btn");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            const current = document.getElementsByClassName("btn-active");
+            current[0].className = current[0].className.replace(" btn-active", "");
+            this.className += " btn-active";
+        });
+    }
+}
+
