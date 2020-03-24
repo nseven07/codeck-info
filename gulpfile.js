80b2192ff3e61dd7d9dbbe4f001f5cf1e71c0269
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-03-24 18:55:39 +0300

    update package and optimize gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,33 @@
 'use strict';
 
 const gulp = require('gulp');
-const fs = require('fs')
-
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 const gsass = require('gulp-sass');
 const cssnano = require('cssnano');
-const postcss = require('postcss');
+const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const twig = require('gulp-twig');
 const concat = require('gulp-concat');
 const connect = require('gulp-connect');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify-es').default;
-const open = require('gulp-open');
 const header = require('gulp-header');
 const imagemin = require('gulp-imagemin');
 const data = require('gulp-data');
+
 const templates = './src/presentation/';
 const dist = './dist/';
 const homePath = './src/assets/';
 const nodeModules = './node_modules/';
-const js = 'assets/js/  ';
-const css = 'assets/css/';
-const icons = 'assets/icons/';
+
 const foreach = require('gulp-foreach');
 
+const paths = {
+    js: 'assets/js/',
+    css: 'assets/css/',
+    icons: 'assets/icons/'
+};
 
 const sourceJs = [
     nodeModules + 'jquery/dist/jquery.js',
@@ -59,9 +60,11 @@ function compile() {
 
 function scripts() {
     return gulp.src(sourceJs)
+        .pipe(sourcemaps.init())
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename('scripts.min.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(dist + 'assets/js'))
         .pipe(connect.reload());
 }
@@ -95,10 +98,17 @@ function copyFiles() {
 }
 
 function style() {
+    const plugins = [
+        autoprefixer(),
+        cssnano({discardComments: true})
+    ];
     return gulp.src(sourceSass)
+        .pipe(sourcemaps.init({}))
         .pipe(gsass({
             includePaths: ['node_modules']
-        }))
+        }).on('error', gsass.logError))
+        .pipe(postcss(plugins))
+        .pipe(sourcemaps.write('.', {addComment: false}))
         .pipe(gulp.dest(dist + 'assets/css'))
         .pipe(connect.reload());
 }
