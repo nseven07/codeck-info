f1a688ef806b887646dfba6868e4a9959776f9a0
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-04-21 12:33:41 +0300

    update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,40 +15,6 @@ $(document).ready(function () {
 
     new WOW({mobile: false}).init();
 
-    $('.staff-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-        speed: 300,
-        autoplay: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-
     $('.projectImages').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
